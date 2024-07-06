1a4d844bb68652c4a6cc87a744532d5352175977
ahqrt <1217223113@qq.com>
2024-07-06 15:21:38 +0800

    Update SQLx queries in main function to include deleting a user with id 1 and updating a user with id 2 to set the username to "victor".

--- examples/sqlx.rs
+++ examples/sqlx.rs
@@ -22,11 +22,11 @@ async fn main() -> Result<()> {
         .max_connections(5)
         .connect(&database_url)
         .await?;
-    let users = sqlx::query_as::<_, User>("SELECT * FROM users")
-        .fetch_all(&pool)
-        .await?;
+    // let users = sqlx::query_as::<_, User>("SELECT * FROM users")
+    //     .fetch_all(&pool)
+    //     .await?;
 
-    println!("{:?}", users);
+    // println!("{:?}", users);
 
     // insert into database users
 
@@ -40,6 +40,20 @@ async fn main() -> Result<()> {
     // .await?;
 
     // println!("{:?}", user);
+
+    // delete user with id 1
+    // sqlx::query("DELETE FROM users WHERE id = $1 RETURNING *")
+    //     .bind(1)
+    //     .execute(&pool)
+    //     .await?;
+
+    // update user with id 2, set username to victor
+
+    sqlx::query("UPDATE users SET username = $1 WHERE id = $2 RETURNING *")
+        .bind("victor")
+        .bind(3)
+        .fetch_one(&pool)
+        .await?;
 
     Ok(())
 }
